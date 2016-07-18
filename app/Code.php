81d82bfede902c4d63ef6b9cf05261eb41d2f1b4
DannyAndres <danny.aguilar@usach.cl>
2016-07-17 23:20:18 -0400

    Ignorar timestamp en relacion muchos a muchos

--- app/Code.php
+++ app/Code.php
@@ -14,6 +14,6 @@ class Code extends Model
 
     public function users()
     {
-        return $this->belongsToMany('App\User');
+        return $this->belongsToMany('App\User')->withTimestamps();
     }
 }
